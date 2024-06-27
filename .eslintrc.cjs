d27add6cbc12d15c8db83aa0e1ddb25cde148b82
Никита <pravoepredserdie@gmail.com>
2024-06-27 21:51:19 +0300

    add eslint, prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,35 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "prettier",
+    "plugin:prettier/recommended"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: "module",
+    project: "./tsconfig.app.json"
+  },
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier', 'import'],
   rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", 'ts', 'tsx'] }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
 }
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <>
+      <div>123</div>
+      <div>Hello world</div>
+    </>
+  );
+}
+
+export default Main;
